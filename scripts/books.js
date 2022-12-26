ce6655e8536de26d2115a089b6f8df5d2cbe5b1e
annavarner <var21033@byui.edu>
2022-12-25 21:30:53 -0500

    first commit

---
+++ scripts/books.js
@@ -0,0 +1,51 @@
+const requestURL = './json/bookdata.json';
+const cards = document.querySelector('.cards');
+
+
+fetch(requestURL)
+
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    const books = jsonObject['books'];
+    books.forEach(displayBooks);
+  });
+
+
+
+  function displayBooks(book) {
+
+    let card = document.createElement('section');
+    let url = document.createElement('a');
+    let h2 = document.createElement('h2');
+    let image = document.createElement('img');
+    let infodiv = document.createElement('div')
+    let date = document.createElement('p');
+
+
+  
+    card.setAttribute('class', 'bookCard');
+    url.setAttribute('href', book.url);
+    h2.textContent = `${book.name}`;
+    infodiv.setAttribute('class', 'infodiv')
+    date.innerHTML = `<strong>Date Published: </strong> ${book.date}`;
+
+    card.appendChild(url);
+    url.appendChild(h2);
+
+  
+
+    image.setAttribute('src', book.img);
+    image.setAttribute('alt', `Book Cover for ${book.name}`);
+    image.setAttribute('loading', 'lazy');
+  
+
+    url.appendChild(image);
+    card.appendChild(infodiv);
+    infodiv.appendChild(date);
+
+
+  
+    document.querySelector('div.cards').appendChild(card);
+  }
---
+++ scripts/changeview.js
@@ -0,0 +1,17 @@
+const gridbutton = document.querySelector("#viewGrid");
+const listbutton = document.querySelector("#viewList");
+const display = document.querySelector("article");
+
+gridbutton.addEventListener("click", () => {
+    display.classList.add("grid")
+    display.classList.remove("list");
+})
+
+listbutton.addEventListener("click", () => {
+    display.classList.add("list");
+    display.classList.remove("grid");
+})
+
+const seriesName = document.querySelector("#series").value;
+
+
